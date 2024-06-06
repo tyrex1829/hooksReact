839e758d576e68b281b43db30569542303933b82
Saksham Khare <sakshamkhare19@gmail.com>
2024-06-06 21:12:31 +0530

    todo component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,31 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import HeaderWithButton from "./components/HeaderWithButton";
+import Todo from "./components/Todo";
 
 function App() {
-  const [name, setName] = useState("Saksham");
+  const [id, setId] = useState(3);
+  const [todo, setTodo] = useState([
+    {
+      id: 1,
+      title: "todo1",
+      description: "This is a description of todo1",
+    },
+    {
+      id: 2,
+      title: "todo2",
+      description: "This is a description of todo2",
+    },
+    {
+      id: 3,
+      title: "todo3",
+      description: "This is a description of todo3",
+    },
+  ]);
 
   return (
     <div>
-      <button
+      {/* <button
         onClick={() => {
           setName(Math.random());
         }}
@@ -20,7 +38,28 @@ function App() {
       <Header x={"Khare"} />
       <Header x={"Khare"} />
       <Header x={"Khare"} />
-      <Header x={"Khare"} />
+      <Header x={"Khare"} /> */}
+
+      <button
+        onClick={() => {
+          setId(id + 1);
+
+          setTodo([
+            ...todo,
+            {
+              id: id + 1,
+              title: Math.random(),
+              description: Math.random(),
+            },
+          ]);
+        }}
+      >
+        Add new todo
+      </button>
+
+      {todo.map((i) => (
+        <Todo title={i.title} description={i.description} />
+      ))}
     </div>
   );
 }
