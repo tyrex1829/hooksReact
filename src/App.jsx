aa69df8e85c7d348b6c2a5ecf5a84bd4c02ca67c
Saksham Khare <sakshamkhare19@gmail.com>
2024-06-06 19:19:02 +0530

    re-render

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@ import "./App.css";
 import Header from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [name, setName] = useState("Saksham");
 
   return (
     <>
-      <Header x={"Saksham"} />
+      <button
+        onClick={() => {
+          setName(Math.random());
+        }}
+      >
+        Click me to change the title
+      </button>
+      <Header x={name} />
       <Header x={"Khare"} />
     </>
   );
