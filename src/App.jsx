bcfa06c6b3c320b240381b4b2bbb710a573598ad
Saksham Khare <sakshamkhare19@gmail.com>
2024-06-11 15:12:32 +0530

    request hit to the todo server

--- src/App.jsx
+++ src/App.jsx
@@ -8,77 +8,21 @@ import TextComponent from "./components/TextComponent";
 import InputComponent from "./components/InputComponent";
 
 function App() {
-  const [id, setId] = useState(3);
-  const [todo, setTodo] = useState([
-    {
-      id: 1,
-      title: "todo1",
-      description: "This is a description of todo1",
-    },
-    {
-      id: 2,
-      title: "todo2",
-      description: "This is a description of todo2",
-    },
-    {
-      id: 3,
-      title: "todo3",
-      description: "This is a description of todo3",
-    },
-  ]);
+  const [todos, setTodos] = useState([]);
 
-  // useEffect
   useEffect(() => {
-    alert("Render");
+    fetch("https://sum-server.100xdevs.com/todos").then(async (res) => {
+      const json = await res.json();
+      setTodos(json.todos);
+    });
   }, []);
 
   return (
     <div>
-      {/* // header */}
-      {/* <button
-        onClick={() => {
-          setName(Math.random());
-        }}
-      >
-        Click me to change title
-      </button>
-      <Header x={name} />
-      <Header x={"Khare"} />
-      <Header x={"Khare"} />
-      <Header x={"Khare"} />
-      <Header x={"Khare"} />
-      <Header x={"Khare"} /> */}
-      {/* // todo */}
-      {/* <button
-        onClick={() => {
-          setId(id + 1);
-
-          setTodo([
-            ...todo,
-            {
-              id: id + 1,
-              title: Math.random(),
-              description: Math.random(),
-            },
-          ]);
-        }}
-      >
-        Add new todo
-      </button>
-
-      {todo.map((i) => (
+      {/* fetch */}
+      {todos.map((i) => (
         <Todo key={i.id} title={i.title} description={i.description} />
-      ))} */}
-      {/* wrapper Component */}
-      {/* <CardWrapper innerComponent={<TextComponent />} />
-      <CardWrapper innerComponent={<InputComponent />} /> */}
-      {/* real code of wrapper */}
-      {/* <CardWrapper>
-        This is first inner text for wrapper. Here we don't do it explicitly.
-      </CardWrapper>
-      <CardWrapper>This is second inner Text for wrapper.</CardWrapper> */}
-      {/* useEffect text */}
-      Checking if above alert is render only once. This text is useless.
+      ))}
     </div>
   );
 }
